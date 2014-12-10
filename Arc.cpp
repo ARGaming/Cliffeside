8bece9667759b69caf70b25ff461ab3a23cc7063
Zephilinox <Zephilinox@hotmail.co.uk>
2014-12-10 18:37:42 +0000

    Added comments, renamed functions to ar, and implemented collision points

--- Arc.cpp
+++ Arc.cpp
@@ -14,7 +14,7 @@ Arc::Arc(float speed, float range, float length)
 }
 
 
-void Arc::update(float dt)
+void Arc::arUpdate(float dt)
 {
     if (m_swinging)
     {
@@ -37,19 +37,35 @@ void Arc::draw(sf::RenderTarget& target, sf::RenderStates states) const
     target.draw(m_collider, states);
 }
 
-void Arc::startSwing(float angle)
+void Arc::arStartSwing(float angle)
 {
     m_swinging = true;
     m_beginSwingAngle = angle;
     m_collider.setRotation(angle);
 }
 
-void Arc::setPosition(const sf::Vector2f& pos)
+void Arc::arSetPosition(const sf::Vector2f& pos)
 {
     m_collider.setPosition(pos);
 }
 
-sf::FloatRect Arc::getCollider()
+std::vector<sf::Vector2f> Arc::arGetCollisionPoints()
 {
-    return sf::FloatRect(m_collider.getPosition(), m_collider.getSize());
+    std::vector<sf::Vector2f> collisionPoints;
+    float radians = m_collider.getRotation() * (3.141592653 / 180.f); //move to Math at some point
+    sf::Vector2f normAngleVec(std::cos(radians), std::sin(radians)); //turn radians in to a unit vector
+
+    for (int i = 0; i < m_collider.getSize().x; i += 20) //Return positions in steps of 10 pixels
+    {
+        sf::Vector2f colPos(normAngleVec.x * i, normAngleVec.y * i);
+        colPos += m_collider.getPosition();
+
+        collisionPoints.push_back(colPos);
+    }
+
+    //collider Size X is the length of the arc
+    collisionPoints.push_back(sf::Vector2f(m_collider.getPosition().x + normAngleVec.x * m_collider.getSize().x,
+                                           m_collider.getPosition().y + normAngleVec.y * m_collider.getSize().x));
+
+    return collisionPoints;
 }
--- Arc.h
+++ Arc.h
@@ -1,20 +1,27 @@
 #ifndef ARC_H
 #define ARC_H
 
+#include <vector>
+
 #include <SFML/Graphics.hpp>
 
 class Arc : public sf::Drawable
 {
 public:
+    //Speed = fraction of range moved each frame, e.g. 1 speed = reach the end in one frame
     Arc(float speed, float range, float length);
-    void update(float dt);
+
+    void arUpdate(float dt);
     void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-    void startSwing(float angle);
+    //Start a swing from the passed angle until the range specified in the constructor
+    void arStartSwing(float angle);
 
-    void setPosition(const sf::Vector2f& pos);
+    void arSetPosition(const sf::Vector2f& pos);
 
-    sf::FloatRect getCollider();
+    //This returns a list of collision points along the current angle of the arc
+    //Use with the contains() function of SFML rects
+    std::vector<sf::Vector2f> arGetCollisionPoints();
 
 private:
     float m_speed;
