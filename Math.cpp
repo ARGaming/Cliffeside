a15d1f47d83951682cc3d3d849ce67bbd08efba0
ARGames <ARGamesCO@gmail.com>
2014-12-15 15:15:47 -0800

    Arc fix - plus clean up
    Math didn't need a class
    Changed the way player rotation is set
    Fixed Arc to work in varying angles and ranges

--- Math.cpp
+++ Math.cpp
@@ -4,29 +4,18 @@
 
 namespace arMath
 {
-    Math::Math()
+    float length(sf::Vector2f vec)
     {
-
+        return sqrtf(vec.x * vec.x + vec.y * vec.y);
     }
 
-    Math::~Math()
+    float dotProduct(sf::Vector2f A, sf::Vector2f B)
     {
 
+        return (A.x * B.x + A.y * B.y);
     }
 
-
-    float Math::length(sf::Vector2f vec)
-    {
-        return (float)sqrtf(vec.x * vec.x + vec.y * vec.y);
-    }
-
-    float Math::dotProduct(sf::Vector2f A, sf::Vector2f B)
-    {
-
-        return (float)(A.x * B.x + A.y * B.y);
-    }
-
-    sf::Vector2f Math::normalize(sf::Vector2f vec)
+    sf::Vector2f normalize(sf::Vector2f vec)
     {
         sf::Vector2f normVec;
         normVec.x = vec.x / length(vec);
@@ -36,7 +25,7 @@ namespace arMath
     }
 
 
-    float Math::angleBetweenVecs(sf::Vector2f A, sf::Vector2f B)
+    float angleBetweenVecs(sf::Vector2f A, sf::Vector2f B)
     {
         float angle = acosf(dotProduct(A, B) / (length(A) * length(B)));
         return angle;
--- Math.h
+++ Math.h
@@ -5,19 +5,10 @@
 
 namespace arMath
 {
-    class Math
-    {
-    public:
-        Math();
-        ~Math();
-        static float dotProduct(sf::Vector2f, sf::Vector2f);
-        static float length(sf::Vector2f);
-        static float angleBetweenVecs(sf::Vector2f, sf::Vector2f);
-        static sf::Vector2f normalize(sf::Vector2f);
-
-    };
-
-
+    float dotProduct(sf::Vector2f, sf::Vector2f);
+    float length(sf::Vector2f);
+    float angleBetweenVecs(sf::Vector2f, sf::Vector2f);
+    sf::Vector2f normalize(sf::Vector2f);
 }
 
 #endif // MATH_H_INCLUDED
